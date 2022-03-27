7024f30af848ece89fb65816b9e5712a4a6cb5fb
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-27 00:04:51 -0500

    Updated query to prepared statement

--- functions/pages/responses.php
+++ functions/pages/responses.php
@@ -1,9 +1,7 @@
 <?php
-// Start the DB connection
 // Delete response from DB
 if (isset($_POST["deleteSubmit"])) {
-    $sql = "DELETE FROM responses WHERE id=$_POST[responseId]";
-    if (mysqli_query($conn, $sql)) {
-    }
+    $stmt = $conn->prepare("DELETE FROM responses WHERE id=?");
+    $stmt->bind_param('s', $_POST["responseId"]);
+    $stmt->execute();
 }
-?>
