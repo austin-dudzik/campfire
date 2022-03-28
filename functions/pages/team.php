6b9c2130c18eafac4a59cdb3a7852d0e0367f2f8
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-27 22:33:42 -0500

    Fixed user create/update query logic

--- functions/pages/team.php
+++ functions/pages/team.php
@@ -2,74 +2,64 @@
 // If profile form is submitted...
 if (isset($_POST["create"])) {
 
-    // Hash the password
-    $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
-
-    // Insert the user into the database
-    $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password, role) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param('ssssi', $_POST["first_name"], $_POST["last_name"], $_POST["email"], $password, $_POST["role"]);
+    $stmt = $conn->prepare("SELECT id FROM users WHERE email = ? LIMIT 1");
+    $stmt->bind_param('s', $_POST["email"]);
     $stmt->execute();
 
-    if ($stmt->affected_rows > 0) {
-        $alert = "Successfully created user!";
+    if ($stmt->get_result()->num_rows == 0) {
+
+        // Close the previous connection
+        $stmt->close();
+
+        // Hash the password
+        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+
+        // Insert the user into the database
+        $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password, role) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param('ssssi', $_POST["first_name"], $_POST["last_name"], $_POST["email"], $password, $_POST["role"]);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            $success = "Successfully created user!";
+        } else {
+            $error = "Sorry, an error has occurred: " . " " . $conn->error;
+        }
+
     } else {
-        echo "Sorry, an error has occurred: " . " " . $conn->error;
+        $error = "Email already exists!";
     }
 
 }
 
 if (isset($_POST['editUser'])) {
 
-    // Set variables for sent values
-    $userId = escape($_POST["user_id"]);
-    $firstName = escape($_POST["first_name"]);
-    $lastName = escape($_POST["last_name"]);
-    $emailAddress = escape($_POST["email"]);
-    $password = $_POST["password"];
-    if (!empty($_POST["password"])) {
-        $password_orig = escape($_POST["password"]);
-        $password = password_hash($password_orig, PASSWORD_BCRYPT);
-    }
-    $role = escape($_POST["role"]);
+    $stmt = $conn->prepare("SELECT id FROM users WHERE email = ? AND id != ? LIMIT 1");
+    $stmt->bind_param('si', $_POST["email"], $_POST["user_id"]);
+    $stmt->execute();
 
-    $sql = "SELECT `id` FROM `users` WHERE `email` = '$emailAddress' AND `id` = '$userId' LIMIT 1";
-    $result = $conn->query($sql);
-    $row = $result->fetch_row();
+    if ($stmt->get_result()->num_rows == 0) {
 
-    if ($result->num_rows > 0) {
+        // Close the previous connection
+        $stmt->close();
 
-        $sql = "UPDATE `users` SET  `first_name` = '$firstName', `last_name` = '$lastName', `email` = '$emailAddress', `password` = IF('" . $password . "' = '', `password`, '" . $password . "'), `role`= $role WHERE id='$_POST[user_id]' LIMIT 1";
+        // Hash password or return empty string if password empty
+        $password = !empty($_POST["password"]) ? password_hash(escape($_POST["password"]), PASSWORD_DEFAULT) : '';
 
-        if ($conn->query($sql) === true) {
+        // Update the user
+        $stmt = $conn->prepare("UPDATE users SET first_name = ?, last_name = ?, email = ?, password = IF(? = '', password, ?), role = ? WHERE id= ? LIMIT 1");
+        $stmt->bind_param('sssssii', $_POST['first_name'], $_POST['last_name'], $_POST['email'], $password, $password, $_POST['role'], $_POST['user_id']);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
             $success = "Successfully updated user!";
-        }
-        else {
-            echo "Sorry, an error occurred: ". " " . $conn->error;
+        } else {
+            $error = "Sorry, an error has occurred: " . " " . $conn->error;
         }
 
+
+    } else {
+        $error = "Email already exists!";
     }
-    else {
 
-        $sql = "SELECT id FROM users WHERE email = '$emailAddress' AND id != $userId LIMIT 1";
-        $result = $conn->query($sql);
-        $row = $result->fetch_row();
-
-        if ($result->num_rows > 0) {
-            $error = "Sorry, that email address is already in use.";
-        }
-
-        else {
-
-            $sql = "UPDATE `users` SET `first_name` = '$firstName', `last_name` = '$lastName', `email` = '$emailAddress', `password` = IF('" . $password . "' = '', `password`, '" . $password . "'), `role`= $role WHERE id='$_POST[user_id]' LIMIT 1";
-
-            if ($conn->query($sql) === true) {
-                $success = "Successfully updated user!";
-            }
-            else {
-                echo "Sorry, an error occurred: " . " " . $conn->error;
-            }
-
-        }
-    }
 
 }
